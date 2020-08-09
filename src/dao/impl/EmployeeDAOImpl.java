cf5510aa37d9c77fef4a1b9fb7f01aa306c85d20
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-09 11:45:23 +0530

    Modified EmployeeDAOImpl according to the employeeDAO with type arguments

--- src/dao/impl/EmployeeDAOImpl.java
+++ src/dao/impl/EmployeeDAOImpl.java
@@ -131,27 +131,131 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public List<Employee> getAll() {
-        return null;
+        ArrayList<Employee> employees = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employee");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                employees.add(new Employee(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getDate(6),
+                        resultSet.getDate(6),
+                        resultSet.getBigDecimal(7),
+                        resultSet.getString(8)
+                ));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  employees;
+
+
     }
 
     @Override
     public Employee get(String pk) {
-        return null;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employee WHERE empId=(?)");
+            preparedStatement.setObject(1,pk);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return new Employee(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getDate(6),
+                        resultSet.getDate(6),
+                        resultSet.getBigDecimal(7),
+                        resultSet.getString(8)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  null;
+
+
+
     }
 
     @Override
     public boolean save(Employee object) {
-        return false;
+        Employee employee1 = (Employee)object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO employee VALUES (?,?,?,?,?,?,?,?,?)");
+            preparedStatement.setObject(1, employee1.getEmpId());
+            preparedStatement.setObject(2, employee1.getEmpName());
+            preparedStatement.setObject(3, employee1.getEmpAddress());
+            preparedStatement.setObject(4, employee1.getEmail());
+            preparedStatement.setObject(5, employee1.getContactNo());
+            preparedStatement.setObject(4, employee1.getDateOfBirth());
+            preparedStatement.setObject(4, employee1.getJoinedDate());
+            preparedStatement.setObject(4, employee1.getSalary());
+            preparedStatement.setObject(4, employee1.getGender());
+
+
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
+
     }
 
     @Override
     public boolean delete(String pk) {
-        return false;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM employee WHERE empId=(?)");
+            preparedStatement.setObject(1, pk);
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
+
     }
 
     @Override
     public boolean update(Employee object) {
+        Employee employee1 = (Employee)object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE employee SET empName=(?),empAddress=(?),email=(?),contactNo=(?),dateOfBirth=(?),joinedDate=(?),salary=(?),gender=(?) WHERE empId=(?)");
+            preparedStatement.setObject(9, employee1.getEmpId());
+            preparedStatement.setObject(1, employee1.getEmpName());
+            preparedStatement.setObject(2, employee1.getEmpAddress());
+            preparedStatement.setObject(3, employee1.getEmail());
+            preparedStatement.setObject(4, employee1.getContactNo());
+            preparedStatement.setObject(5, employee1.getDateOfBirth());
+            preparedStatement.setObject(6, employee1.getJoinedDate());
+            preparedStatement.setObject(7, employee1.getSalary());
+            preparedStatement.setObject(8, employee1.getGender());
+
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
+
+
+
     }
 
     public  String getLastEmployeeId(){
