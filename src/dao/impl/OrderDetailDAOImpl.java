4cd57f052feef0d5f9f86a9bda79d433c34e9d41
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-09 11:52:32 +0530

    Modified OrderDetailDAOImpl according to the orderDetailDAO with type arguments

--- src/dao/impl/OrderDetailDAOImpl.java
+++ src/dao/impl/OrderDetailDAOImpl.java
@@ -120,27 +120,115 @@ public class OrderDetailDAOImpl implements OrderDetailDAO {
 */
     @Override
     public List<OrderDetail> getAll() {
-        return null;
+        ArrayList<OrderDetail> ordersDetails = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM orderdetail");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                ordersDetails.add(new OrderDetail(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getBigDecimal(3),
+                        resultSet.getBigDecimal(4)
+
+                ));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  ordersDetails;
+
+
     }
 
     @Override
     public OrderDetail get(OrderDetailPK pk) {
-        return null;
+        OrderDetailPK orderDetailPK1 = (OrderDetailPK) pk;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM orderdetail WHERE orderId=(?) AND itemCode=(?)");
+            preparedStatement.setObject(1,orderDetailPK1.getOrderId());
+            preparedStatement.setObject(2,orderDetailPK1.getItemCode());
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return new OrderDetail(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getBigDecimal(3),
+                        resultSet.getBigDecimal(4)
+
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  null;
+
+
     }
 
     @Override
     public boolean save(OrderDetail object) {
-        return false;
+        OrderDetail orderDetail1 = (OrderDetail) object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO orderdetail VALUES (?,?,?,?)");
+            preparedStatement.setObject(1, orderDetail1.getOrderDetailPK().getOrderId());
+            preparedStatement.setObject(2, orderDetail1.getOrderDetailPK().getItemCode());
+            preparedStatement.setObject(3, orderDetail1.getQty());
+            preparedStatement.setObject(4, orderDetail1.getUnitPrice());
+
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+
+        }
     }
 
     @Override
     public boolean delete(OrderDetailPK pk) {
-        return false;
+        OrderDetailPK orderDetailPK1 = (OrderDetailPK)pk;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM orderdetail WHERE orderId=(?) AND itemCode=(?)");
+            preparedStatement.setObject(1, orderDetailPK1.getOrderId());
+            preparedStatement.setObject(1, orderDetailPK1.getItemCode());
+
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
+
     }
 
     @Override
     public boolean update(OrderDetail object) {
+        OrderDetail orderDetail1 = (OrderDetail)object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE orderdetail SET qty=(?),unitPrice=(?) WHERE orderId=(?) AND itemCode=(?)");
+            preparedStatement.setObject(1, orderDetail1.getQty());
+            preparedStatement.setObject(2, orderDetail1.getUnitPrice());
+            preparedStatement.setObject(3, orderDetail1.getOrderDetailPK().getOrderId());
+            preparedStatement.setObject(3, orderDetail1.getOrderDetailPK().getItemCode());
+
+
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
+
     }
 
 
