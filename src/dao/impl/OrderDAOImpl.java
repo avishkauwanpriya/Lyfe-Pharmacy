258acdcb36e999f62a3ce77c14bde3a4507412f8
Avishka Uwanpriya <avishkauwanpriya912@gmail.com>
2020-08-09 11:50:05 +0530

    Modified OrderDAOImpl according to the orderDAO with type arguments

--- src/dao/impl/OrderDAOImpl.java
+++ src/dao/impl/OrderDAOImpl.java
@@ -110,28 +110,110 @@ public class OrderDAOImpl implements OrderDAO{
 
 
     @Override
-    public List<Order> getAll() {
-        return null;
+    public List<Order> getAll(){
+        ArrayList<Order> orders = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `order`");
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                orders.add(new Order(resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3)
+
+                ));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  orders;
+
     }
 
     @Override
     public Order get(String pk) {
-        return null;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM `order` WHERE orderId=(?)");
+            preparedStatement.setObject(1,pk);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return new Order(
+                        resultSet.getString(1),
+                        resultSet.getString(2),
+                        resultSet.getDate(3)
+
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return  null;
+        }
+        return  null;
+
+
     }
 
     @Override
     public boolean save(Order object) {
-        return false;
+        Order order1 = (Order)object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO `order` VALUES (?,?,?)");
+            preparedStatement.setObject(1, order1.getOrderId());
+            preparedStatement.setObject(2, order1.getEmpId());
+            preparedStatement.setObject(3, order1.getOrderDate());
+
+
+
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
+
     }
 
     @Override
     public boolean delete(String pk) {
-        return false;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM `order` WHERE orderId=(?)");
+            preparedStatement.setObject(1, pk);
+            return preparedStatement.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+
+        }
+
+
     }
 
     @Override
     public boolean update(Order object) {
+        Order order1 = (Order)object;
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE `order` SET empId=(?),orderDate=(?) WHERE orderId=(?)");
+            preparedStatement.setObject(1, order1.getEmpId());
+            preparedStatement.setObject(2, order1.getOrderDate());
+            preparedStatement.setObject(3, order1.getOrderId());
+
+
+            return preparedStatement.executeUpdate()>0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
+
+
     }
 
     public  String getLastOrderId(){
